27a0eb813e9951292bf4bac21ff8f7ed2d3f05c9
Bishal Rana <rbishal50@gmail.com>
2021-03-07 21:39:38 +0545

    Prepare app for deployment

--- models/tourModel.js
+++ models/tourModel.js
@@ -163,10 +163,10 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds to execute`);
-  next();
-});
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} milliseconds to execute`);
+//   next();
+// });
 
 // aggregation middleware
 // tourSchema.pre('aggregate', function (next) {
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,11 +6,7 @@ const stripe = Stripe('pk_test_3nJP0W13GmBRgd1oFg0eqA1U00sxRCmi1B');
 export const bookTour = async (tourId) => {
   try {
     // 1. get checkout session from the api
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2. create checkout form + charge cedit card
     await stripe.redirectToCheckout({
